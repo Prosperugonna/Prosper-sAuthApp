26f8de1f4b0eaf18585f44fb0eddeac07c9c4552
Prosperugonna <prosperugonna@gmail.com>
2023-12-28 14:02:16 +0100

    FirebaseConfiguration

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-auth-1c4ae.firebaseapp.com',
-  projectId: 'mern-auth-1c4ae',
-  storageBucket: 'mern-auth-1c4ae.appspot.com',
-  messagingSenderId: '277641423672',
-  appId: '1:277641423672:web:2de25252aae022d51aafcd',
+  authDomain: "prosper-sauthapp.firebaseapp.com",
+  projectId: "prosper-sauthapp",
+  storageBucket: "prosper-sauthapp.appspot.com",
+  messagingSenderId: "8838680358",
+  appId: "1:8838680358:web:2e5d4672b663cbce7d3be7"
 };
 
 // Initialize Firebase
